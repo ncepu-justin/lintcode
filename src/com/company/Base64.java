8ae97fc3151cba95d511c50ce5588ae261a95919
justin <495778503@qq.com>
2019-09-03 20:29:06 +0800

    字符串处理

--- src/com/company/Base64.java
+++ src/com/company/Base64.java
@@ -1,8 +1,10 @@
 package com.company;
 
-import sun.misc.BASE64Encoder;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Justin
@@ -11,56 +13,87 @@ import java.io.UnsupportedEncodingException;
 public class Base64 {
 
 
+    private static final String base64Code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-    private static final String base64Code= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     public String base64Encoder(String srcStr) throws UnsupportedEncodingException {
         //有效值检查
-        if(srcStr == null || srcStr.length() == 0) {
+        if (srcStr == null || srcStr.length() == 0) {
             return srcStr;
         }
         //将明文的ASCII码转为二进制位字串
-        char[] srcStrCh= srcStr.toCharArray();
-        StringBuilder asciiBinStrB= new StringBuilder();
-        String asciiBin= null;
-        for(int i= 0; i< srcStrCh.length; i++) {
-            asciiBin= Integer.toBinaryString((int)srcStrCh[i]);
-            while(asciiBin.length()< 8) {
-                asciiBin= "0"+ asciiBin;
+        char[] srcStrCh = srcStr.toCharArray();
+        StringBuilder asciiBinStrB = new StringBuilder();
+        String asciiBin = null;
+        for (int i = 0; i < srcStrCh.length; i++) {
+            asciiBin = Integer.toBinaryString((int) srcStrCh[i]);
+            while (asciiBin.length() < 8) {
+                asciiBin = "0" + asciiBin;
             }
             asciiBinStrB.append(asciiBin);
         }
         //跟据明文长度在二进制位字串尾部补“0”
-        while(asciiBinStrB.length()% 6!= 0) {
+        while (asciiBinStrB.length() % 6 != 0) {
             asciiBinStrB.append("0");
         }
-        String asciiBinStr= String.valueOf(asciiBinStrB);
+        String asciiBinStr = String.valueOf(asciiBinStrB);
         //将上面得到的二进制位字串转为Value，再跟据Base64编码表将之转为Encoding
-        char[] codeCh= new char[asciiBinStr.length()/ 6];
-        int index= 0;
-        for(int i= 0; i< codeCh.length; i++) {
-            index= Integer.parseInt(asciiBinStr.substring(0, 6), 2);
-            asciiBinStr= asciiBinStr.substring(6);
-            codeCh[i]= base64Code.charAt(index);
+        char[] codeCh = new char[asciiBinStr.length() / 6];
+        int index = 0;
+        for (int i = 0; i < codeCh.length; i++) {
+            index = Integer.parseInt(asciiBinStr.substring(0, 6), 2);
+            asciiBinStr = asciiBinStr.substring(6);
+            codeCh[i] = base64Code.charAt(index);
         }
-        StringBuilder code= new StringBuilder(String.valueOf(codeCh));
+        StringBuilder code = new StringBuilder(String.valueOf(codeCh));
         //跟据需要在尾部添加“=”
-        if(srcStr.length()% 3 == 1) {
+        if (srcStr.length() % 3 == 1) {
             code.append("==");
-        } else if(srcStr.length()% 3 == 2) {
+        } else if (srcStr.length() % 3 == 2) {
             code.append("=");
         }
         //每76个字符加一个回车换行符（CRLF）
-        int i= 76;
-        while(i< code.length()) {
+        int i = 76;
+        while (i < code.length()) {
             code.insert(i, "\r\n");
-            i+= 76;
+            i += 76;
         }
         code.append("\r\n");
         return String.valueOf(code);
     }
 
-    public static void main (String[] args) throws UnsupportedEncodingException {
-        Base64 base64=new Base64();
-        System.out.println( base64.base64Encoder("Man"));
+    //a<<b((c)<)
+    public String deleteStr(String src) {
+        if (src == null || src.length() == 0)
+            return "";
+        int len = src.length();
+        List<Boolean> isValid=new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (isValid.isEmpty() && src.charAt(i) != '<' && src.charAt(i) != '(' && src.charAt(i) != ')') {
+                stack.push(src.charAt(i));
+            } else if (isValid.isEmpty() && src.charAt(i) == '<') {
+                if (stack.isEmpty()) continue;
+                stack.pop();
+            } else if (src.charAt(i) == '(') {
+                isValid.add(false);
+            } else if(src.charAt(i) == ')') {
+                isValid.remove(isValid.size()-1);
+            }
+        }
+        StringBuilder temp = new StringBuilder();
+        while (!stack.isEmpty()) {
+            temp.append(stack.pop());
+        }
+        StringBuilder res = new StringBuilder();
+        for (int i = temp.length() - 1; i >= 0; i--) {
+            res.append(temp.charAt(i));
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        Base64 base64 = new Base64();
+        String res=base64.deleteStr("a<<b((c)<)");
+        System.out.println(res);
     }
 }
